ccd6b480dc1aab68f132c61e1dc583fef5defe32
Osman Semir <osmanshek@gmail.com>
2025-03-01 14:41:13 +0400

    Upgrade to harpoon 2

--- .config/nvim/lua/osman/plugins/harpoon.lua
+++ .config/nvim/lua/osman/plugins/harpoon.lua
@@ -1,56 +1,44 @@
 return {
 	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+
 	config = function()
-		require("harpoon").setup({})
+		local harpoon = require("harpoon")
+		harpoon.setup({})
+
 		local keymap = vim.keymap
 
-		keymap.set(
-			"n",
-			"<leader>rr",
-			':lua require("harpoon.ui").toggle_quick_menu()<CR>',
-			{ desc = "Toggle Harpoon Menu" }
-		)
-		keymap.set(
-			"n",
-			"<leader>rm",
-			':lua require("harpoon.mark").add_file()<CR>',
-			{ desc = "Add File to Harpoon Menu" }
-		)
-		keymap.set(
-			"n",
-			"<a-1>",
-			':lua require("harpoon.ui").nav_file(1)<CR>',
-			{ desc = "Navigate to File 1 in Harpoon Menu" }
-		)
-		keymap.set(
-			"n",
-			"<a-2>",
-			':lua require("harpoon.ui").nav_file(2)<CR>',
-			{ desc = "Navigate to File 2 in Harpoon Menu" }
-		)
-		keymap.set(
-			"n",
-			"<a-3>",
-			':lua require("harpoon.ui").nav_file(3)<CR>',
-			{ desc = "Navigate to File 3 in Harpoon Menu" }
-		)
-		keymap.set(
-			"n",
-			"<a-4>",
-			':lua require("harpoon.ui").nav_file(4)<CR>',
-			{ desc = "Navigate to File 4 in Harpoon Menu" }
-		)
-		keymap.set(
-			"n",
-			"<a-j>",
-			':lua require("harpoon.ui").nav_prev()<CR>',
-			{ desc = "Navigate to Previous File in Harpoon Menu" }
-		)
-		keymap.set(
-			"n",
-			"<a-k>",
-			':lua require("harpoon.ui").nav_next()<CR>',
-			{ desc = "Navigate to next File in Harpoon Menu" }
-		)
+		keymap.set("n", "<leader>rr", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Toggle Harpoon Menu" })
+
+		keymap.set("n", "<leader>rm", function()
+			harpoon:list():add()
+		end, { desc = "Add File to Harpoon Menu" })
+
+		keymap.set("n", "<a-1>", function()
+			harpoon:list():select(1)
+		end, { desc = "Navigate to File 1 in Harpoon Menu" })
+
+		keymap.set("n", "<a-2>", function()
+			harpoon:list():select(2)
+		end, { desc = "Navigate to File 2 in Harpoon Menu" })
+
+		keymap.set("n", "<a-3>", function()
+			harpoon:list():select(3)
+		end, { desc = "Navigate to File 3 in Harpoon Menu" })
+
+		keymap.set("n", "<a-4>", function()
+			harpoon:list():select(4)
+		end, { desc = "Navigate to File 4 in Harpoon Menu" })
+
+		keymap.set("n", "<a-j>", function()
+			harpoon:list():prev()
+		end, { desc = "Navigate to Previous File in Harpoon Menu" })
+
+		keymap.set("n", "<a-k>", function()
+			harpoon:list():next()
+		end, { desc = "Navigate to next File in Harpoon Menu" })
 	end,
 }
